6cb61e737acd6705249128f4d7c2f52937566d87
Judah Axelrod <64099637+judah-axelrod@users.noreply.github.com>
2020-08-12 14:41:18 -0400

    Add files via upload

---
+++ Situational-Pitching/Code/multi_spread.R
@@ -0,0 +1,12 @@
+#Use this to transpose with 1 key and multiple values. See "Situational Pitching to Contact.R" for example
+
+multi_spread <- function(df, key, value) {
+  # quote key
+  keyq <- rlang::enquo(key)
+  # break value vector into quotes
+  valueq <- rlang::enquo(value)
+  s <- rlang::quos(!!valueq)
+  df %>% gather(variable, value, !!!s) %>%
+    unite(temp, !!keyq, variable) %>%
+    spread(temp, value)
+}
